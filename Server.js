3f6e3f70b34baa77c47001119b70a4bda0a54219
htnsantos <helton.santos2009@hotmail.com>
2017-10-17 18:37:53 -0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -2,7 +2,4 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
-app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/src/index.html'));
-});
 app.listen(process.env.PORT || 8080);
