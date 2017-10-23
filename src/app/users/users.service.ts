6452b6bf3ca6f8072af6927d2f5aa253622abfda
Helton Santos <helton.santos2009@hotmail.com>
2017-10-22 23:16:31 -0200

    ajuste na api de chamado

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -73,7 +73,7 @@ export class UsersService {
 
       console.log(params);
 
-      //this.http.post("https://speeds-api.herokuapp.com/api/message", params, options).subscribe(res => console.log(res.json()));
+      this.http.post("https://speeds-api.herokuapp.com/api/message", params, options).subscribe(res => console.log(res.json()));
       //.map(this.onLoginSuccess)
       //.catch(this.onError);
       var obj = {
